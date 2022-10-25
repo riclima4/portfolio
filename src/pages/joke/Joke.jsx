56a20468e2bf21e0e2be7079852f46adc7031ea5
Ricardo Lima <ric.lima1@hotmail.com>
2022-10-25 21:06:58 +0100

    joke kinda done

--- src/pages/joke/Joke.jsx
+++ src/pages/joke/Joke.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Contact from '../../components/contact/Contact'
+import Footer from '../../components/footer/Footer'
+import './joke.css'
+import axios from 'axios'
 
 export default function Joke() {
+  const url = 'https://official-joke-api.appspot.com/random_joke'
+  
+
+  const [joke, setJoke] = useState([]);
+
+  const getData = async () =>{
+    const res = await axios.get(url)
+    if(!res) return
+    console.log(res.data)
+    setJoke(res.data)
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+
   return (
-    <div>Joke</div>
+    <>
+    <section className="home section" id="home">
+        <div className="joke__container container grid">
+          <h1 className="section__title">A Joke for you!</h1>
+          <p className='joke__description' key={joke.id}>{joke.setup} - {joke.punchline}</p>
+          <button className='button centerText' onClick={()=>{getData()}}>Another one</button>
+        </div>
+        
+    </section>
+    <Contact/>
+    <Footer/>
+    </>
   )
 }
--- src/pages/pokeapi/Pokeapi.jsx
+++ src/pages/pokeapi/Pokeapi.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Footer from '../../components/footer/Footer'
+import './pokeapi.css'
+import axios from 'axios'
 
 export default function pokeapi() {
+  // const url = 'https://official-joke-api.appspot.com/random_joke'
+  
+
+  // const [joke, setJoke] = useState([]);
+
+  // const getData = async () =>{
+  //   const res = await axios.get(url)
+  //   if(!res) return
+  //   console.log(res.data)
+  //   setJoke(res.data)
+  // }
+
+  // useEffect(()=>{
+  //   getData();
+  // },[])
+
+
   return (
-    <h1>pokeapi</h1>
+    <>
+    {/* <section className="home section" id="home">
+        <div className="joke__container container grid">
+          <h1 className="section__title">Pokedex!</h1>
+          <p className='joke__description' key={joke.id}>{joke.setup} - {joke.punchline}</p>
+          <button className='button centerText' onClick={()=>{getData()}}>Another one</button>
+        </div>
+        
+    </section> */}
+    <Footer/>
+    </>
   )
 }
